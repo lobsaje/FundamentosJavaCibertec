3c8f48ef8f423e5b48e7fed8a9e6d83ddb2b8c88
Adminfoco <Adminfoco@10.0.0.6>
2020-12-21 22:07:16 -0500

    Prueba inicial

---
+++ src/main/java/com/mycompany/holamundomaven/HolaMundo.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.holamundomaven;
+
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        //p + control y barra espacio
+        System.out.println("Hola Mundo");
+    }
+}
